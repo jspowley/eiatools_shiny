54e8cfcd7f6f2cddc3c2795af3729b26fd733d92
jspowley <jpowley@ualberta.ca>
2025-02-01 10:14:16 -0700

    rudimentary facet framework, requires feeding back inputs dynamically

--- global.R
+++ global.R
@@ -9,10 +9,17 @@ unique_facets <- function(data_index_table){
   data_index_table %>% pull(facets) %>% unlist() %>% unique()
 }
 
+# Why? Since EIA doesn't actually tell us the mapping of ID to the more descriptive term in the metadata request, 
+# manually mapping is required. Fortunately we only need to maintain about 1-30 columns per table, which isn't ideal, 
+# but certainly isn't into the range of "tasks which are too big for humans"
+
 facet_desc_map <- tibble::tribble(
   ~table, ~facet, ~desc,
+  
   "petroleum", "series", "series-description",
   "petroleum", "duoarea", "area-name",
   "petroleum", "process", "process-name",
   "petroleum", "product", "product-name",
+  
+  "steo", "seriesId", "seriesDescription"
 )
--- server.R
+++ server.R
@@ -5,18 +5,20 @@ server <- function(input, output) {
   bslib::bs_themer()
   
   output$displayed_table <- renderDT(eiatools::data_index[(eiatools::data_index %>% names()) == "petroleum"] %>% .[[1]])
+  r <- shiny::reactiveValues()
+  input_chain <- 
   
   # When a table is selected, we want to display that table, and load it's facet input panels.
   shiny::observeEvent(input$table_select, {
     
     # Using the descriptive name, find the table id associated to the API. Then query the selected table for display
+    print(input$table_select)
+    
     table_id <- eiatools::app_dictionary$tables %>% dplyr::filter(route_1_name == input$table_select) %>% dplyr::pull(route_1_id)
     table_init <- eiatools::data_index[(eiatools::data_index %>% names()) == table_id] %>% .[[1]]
-    output$displayed_table <- renderDT(datatable(
-                                      table_init,
-                                      editable = list(target = "cell", disable = list(columns = c(1, 2))),
-                                      selection = 'multiple'
-                                      ))
+    output$displayed_table <- renderDT(table_init)
+    
+    print("Rendered DT")
     
     # Rendering the frequency options
     freqs <- table_init %>% pull(freq) %>% unique()
@@ -28,44 +30,68 @@ server <- function(input, output) {
                          choices = freqs)
     })
     
+    print("Rendered Frequency")
+    r$table <- table_init
+    r$table_id <- table_id
+    
+  })
+  
+  shiny::observeEvent(r$table, {
+    
+    print("Table Changes Occured, Updating")
     # Rendering the facet options
-    facets <- unique_facets(table_init)
+    facets <- unique_facets(r$table)
     facet_ui <- list()
     facet_schema <- list()
+    
+    print("Beginning Facet Mapping")
+    print("Available facets are:")
+    print(facets)
+    print("Entering Loop")
     
     for(i in 1:length(facets)){
       
       f <- facets[[i]]
-      # print(f)
+      print(paste0("  ",f))
       
       facet_options <- list()
-      f_desc <- facet_desc_map %>% dplyr::filter(table == table_id & facet == f) %>% dplyr::pull(desc)
+      f_desc <- facet_desc_map %>% dplyr::filter(table == r$table_id & facet == f) %>% dplyr::pull(desc)
+      print("    Descriptions Mapped")
       
       # print(f_desc)
       
       facet_options_df <- 
-        table_init %>% 
+        r$table %>% 
         dplyr::select(dplyr::any_of(c(f, f_desc))) %>% 
         dplyr::distinct() %>% 
         dplyr::group_by(!!sym(f_desc)) %>% 
         dplyr::summarise(!!sym(f) := list(list(unique(!!sym(f)))), .groups = "keep") %>%  # Annoyingly enough, 1:many relationships exist, especially where multiple codes exist for the same PADD.
-        dplyr::ungroup()
+        dplyr::ungroup() #%>% 
+        #head(5)
         
       # print(str(facet_options_df %>% head(3)))
       # print("done")
       
       facet_map <- facet_options_df %>% dplyr::select(dplyr::any_of(f)) %>% dplyr::pull(f)
       names(facet_map) <- facet_options_df %>% dplyr::select(dplyr::any_of(f_desc)) %>% dplyr::pull(f_desc)
-      facet_map <- append(facet_map, list("(None)" = NA))
+      facet_map <- append(list("(None)" = NA), facet_map)
       
       # print("assignment")
       facet_ui[[i]] <- shiny::selectizeInput(inputId = f,
                                             label = paste0(stringr::str_to_title(f),":"),
-                                            choices = names(facet_map))
+                                            choices = names(facet_map),
+                                            selected = "(None)")
       # print("exit")
     }
     
     output$facet_ui <- shiny::renderUI({facet_ui})
+    r$facets <- facets
+    
+  })
+  
+  observeEvent(r$facets, {
+    print("FACETS CHANGED")
+  })
     
     # For dynamic reference to the facet !!inputs!!, use input[names(input) == "key"]
     # Make sure there's a (none) option and it's selected by default.
@@ -79,7 +105,7 @@ server <- function(input, output) {
     #    shiny::selectInput
     #    )
     #}
-  })
+  
   
   
 }
